7a050dadc6848253ac0cc6712c3ffe0c450aa8f8
Kezia Regina <keziaregina28@gmail.com>
2025-03-18 09:45:19 +0700

    fix: layouts

--- app/Console/Commands/SendReportMail/Daily.php
+++ app/Console/Commands/SendReportMail/Daily.php
@@ -2,13 +2,16 @@
 
 namespace App\Console\Commands\SendReportMail;
 
-use App\Mail\Reporting;
-use App\Mail\Reporting2;
-use App\ReportSettings;
 use App\User;
+use App\Mail\Reporting;
+use App\ReportSettings;
+use App\Mail\Reporting2;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 class Daily extends Command
 {
@@ -36,24 +39,33 @@ class Daily extends Command
     public function handle()
     {
         $datas = ReportSettings::where('interval', 'daily')->get();
+
+        Log::info("REPORT DATA----------------------------");
+        Log::info(json_encode($datas,JSON_PRETTY_PRINT));
+        Log::info("REPORT DATA----------------------------");
+        
         foreach ($datas as $data) {
-            // if ($data->type === 'purchase_n_sell_report') {
+            if ($data->type === 'purchase_n_sell_report') {
                 $user = User::find($data->user_id);
                 $image = public_path('img/logo-small.png');
-                $filename = storage_path('app/public/pdf/report/Ajyal Al-Madina.pdf');
-                $directory = dirname($filename);
+                $filename = 'Purchase_Sales_Report.pdf';
 
-                if (!file_exists($directory)) {
-                    mkdir($directory, 0777, true);
+                if (!File::exists(public_path('storage/report'))) {
+                    File::makeDirectory(public_path('storage/report'), 0777, true, true);
                 }
+
+                if (File::exists(public_path('storage/report/'.$filename))) {
+                    File::delete(public_path('storage/report/'.$filename));
+                }
+
+              
+                $pdf = Pdf::loadView('report_settings/export/purchase_sales', compact('data', 'image', 'user'))
+                ->setPaper([0, 0, 70.88, 141.75], 'landscape')
+                ->download('Purchase_Sales_Report.pdf');
                 
-                $pdf = Pdf::setPaper('a4', 'landscape')->loadView('report_settings/export/purchase_sales', ['data' => $data, 'image' => $image, 'user' => $user ]);
-                
-                $pdf->save($filename);
                 Mail::to($user->email)->queue(new Reporting($data, $filename));
-            // } else {
-            //     return 'wkwkw';
-            // }
+       
+            }
         }
     }
 
