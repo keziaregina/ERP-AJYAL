748b36688b9d2e020648744ba97fbb5253cb091d
Kezia Regina <keziaregina28@gmail.com>
2025-03-23 23:53:26 +0700

    add: lang translates

--- app/ReportSettings.php
+++ app/ReportSettings.php
@@ -14,6 +14,63 @@ class ReportSettings extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    protected function getReportTypes()
+    {
+        switch($this->type){
+            case 'profit_or_loss_report':
+                return 'Profit / Loss Report';
+            case 'purchase_n_sell_report':
+                return 'Purchase & Sell Report';
+            case 'contacts_report':
+                return 'Contacts Report';
+            case 'customer_n_supplier_report':
+                return 'Customer & Supplier Report';
+            case 'customer_group_report':
+                return 'Customer Group Report';
+            case 'stock_report':
+                return 'Stock Report';
+            case 'stock_adjustment_report':
+                return 'Stock Adjustment Report';
+            case 'tax_report':
+                return 'Tax Report';
+            case 'trending_product_report':
+                return 'Trending Product Report';
+            case 'items_report':
+                return 'Items Report';
+            case 'product_purchase_report':
+                return 'Product Purchase Report';
+            case 'product_sell_report':
+                return 'Product Sell Report';
+            case 'purchase_payment_report':
+                return 'Purchase Payment Report';
+            case 'sales_representative':
+                return 'Sales Representative Report';
+            case 'register_report':
+                return 'Register Report';
+            case 'expense_report':
+                return 'Expense Report';
+            case 'activity_log':
+                return 'Activity Log';
+        }
+    }
+
+    protected function getReportIntervals()
+    {
+        switch($this->interval){
+            case 'daily':
+                return __('report_settings.daily');
+            case 'weekly':
+                return __('report_settings.weekly');
+            case 'monthly':
+                return __('report_settings.monthly');
+            case 'yearly':
+                return __('report_settings.yearly');
+            default:
+                return $this->interval;
+        }
+    }
+
     public static function getUsers($business_id)
     {
         return ReportSettings::with('user')
@@ -23,8 +80,8 @@ class ReportSettings extends Model
                 return [
                     'id' => $reportSetting->id,
                     'user_name' => $reportSetting->user->first_name,
-                    'type' => $reportSetting->type,
-                    'interval' => $reportSetting->interval,
+                    'type' => $reportSetting->getReportTypes(),
+                    'interval' => $reportSetting->getReportIntervals(),
                 ];
             });
     }
--- lang/ar/report_settings.php
+++ lang/ar/report_settings.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'report_settings' => 'إعدادات التقارير',
-    'manage_your_report_settings' => 'إدارة إعدادات التقارير الخاصة بك',
+    'report_settings' => 'إعدادات الإشعارات البريدية',
+    'manage_your_report_settings' => 'إدارة إعدادات الإشعارات البريدية الخاصة بك',
+    'all_your_report_settings' => 'جميع إعدادات الإشعارات البريدية الخاصة بك',
     'user_name' => 'اسم المستخدم',
     'report_type' => 'نوع التقرير',
     'report_interval' => 'فترة التقرير',
@@ -10,4 +11,9 @@ return [
     'added_success' => 'تم إضافة إعداد التقرير بنجاح',
     'updated_success' => 'تم تحديث إعداد التقرير بنجاح',
     'deleted_success' => 'تم حذف إعداد التقرير بنجاح',
+
+    'daily' => 'تقرير يومي',
+    'weekly' => 'تقرير إسبوعي',
+    'monthly' => 'تقرير شهري',
+    'yearly' => 'تقرير سنوي',
 ];
