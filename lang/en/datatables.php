35357180534802c8192461a178cc066487b08c19
Kezia Regina <keziaregina28@gmail.com>
2025-04-02 22:48:31 +0700

    fix: arabic languange support pdf export datatables

---
+++ lang/en/datatables.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'time_export' => 'Exported at',
+    'by_export' => 'Exported by',
+
+    'start_date' => 'Report Start',
+    'end_date' => 'Report End',
+
+    'no_data' => 'No available data',
+    
+    'am' => 'AM',
+    'pm' => 'PM',
+];
