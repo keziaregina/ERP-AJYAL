6080aa3709123d1f93d67dc1346b9f817a8e355e
Kezia Regina <keziaregina28@gmail.com>
2025-04-23 22:29:15 +0700

    WIP: deduction payroll

--- Modules/Essentials/Resources/views/payroll/allowance_and_deduction_row.blade.php
+++ Modules/Essentials/Resources/views/payroll/allowance_and_deduction_row.blade.php
@@ -21,12 +21,23 @@
         {!! Form::text($name_col . '[]', !empty($name) ? $name : null, ['class' => 'form-control input-sm' ]); !!}
     </td>
     <td>
-        {!! Form::select($type_col . '[]', ['fixed' => __('lang_v1.fixed'), 'percent' => __('lang_v1.percentage'), 'overtime' => __('lang_v1.overtime')], $amount_type, ['class' => 'form-control input-sm amount_type' ]); !!}
+        {!! Form::select($type_col . '[]', ['fixed' => __('lang_v1.fixed'), 'percent' => __('lang_v1.percentage')], $amount_type, ['class' => 'form-control input-sm amount_type' ]); !!}
+        {{-- {!! Form::select($type_col . '[]', ['fixed' => __('lang_v1.fixed'), 'percent' => __('lang_v1.percentage'), 'overtime' => __('lang_v1.overtime')], $amount_type, ['class' => 'form-control input-sm amount_type' ]); !!} --}}
         <div class="input-group percent_field @if($amount_type != 'percent') hide @endif">
             {!! Form::text($percent_col . '[]', @num_format($percent), ['class' => 'form-control input-sm input_number percent']); !!}
             <span class="input-group-addon"><i class="fa fa-percent"></i></span>
         </div>
     </td>
+    
+    {{-- Overtime Hours --}}
+    <td>
+        @php
+            $readonly = $amount_type == 'percent' ? 'readonly' : '';
+        @endphp
+        {!! Form::text($val_col . '[]',  0, ['class' => 'form-control input-sm value_field input_number ' . $val_class, $readonly ]); !!}
+        {{-- {!! Form::text($val_col . '[]', !empty($value) ? @num_format((float) $value) : 0, ['class' => 'form-control input-sm value_field input_number ' . $val_class, $readonly ]); !!} --}}
+    </td>
+
     <td>
         @php
             $readonly = $amount_type == 'percent' ? 'readonly' : '';
