c1ea459c8cc67dd36f40ab6ffb838d4970c3a945
Kezia Regina <keziaRegina28@gmail.com>
2025-06-02 14:02:31 +0700

    fix: hide modal select Glorious Employee in Overtime Sheets

--- Modules/Essentials/Resources/views/overtime_sheets/index.blade.php
+++ Modules/Essentials/Resources/views/overtime_sheets/index.blade.php
@@ -112,7 +112,7 @@
             </div>
 
             {{-- Select Employee Glorious --}}
-            <div class="tw-mt-6 tw-p-4 tw-bg-gray-50 tw-rounded-lg tw-border tw-border-gray-200">
+            {{-- <div class="tw-mt-6 tw-p-4 tw-bg-gray-50 tw-rounded-lg tw-border tw-border-gray-200">
 
                 <div class="tw-flex tw-justify-between">
                     <h4 class="tw-text-lg tw-font-semibold tw-mb-3 tw-text-gray-700">@lang('essentials::lang.employee_glorious_this_month')</h4>
@@ -139,7 +139,7 @@
                         <p>-</p>
                     @endif
                 </div>
-            </div>
+            </div> --}}
 
 
         @endcomponent
